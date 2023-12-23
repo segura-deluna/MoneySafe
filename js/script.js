8fcb322682cce30089fbde11e6d73514827bb3ec
Segura <segura-deluna@mail.ru>
2023-12-23 18:24:23 +0300

    connect API

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 import { convertStringNumber } from "./convertStringNumber.js";
 import { OverlayScrollbars } from "./overlayscrollbars.esm.min.js";
 
+const API_URL = "https://quickest-workable-gigantspinosaurus.glitch.me/api";
+
+const typeOperation = {
+  income: "доход",
+  expenses: "расход",
+};
+
 const financeForm = document.querySelector(".finance__form");
 const financeAmount = document.querySelector(".finance__amount");
 const financeReport = document.querySelector(".finance__report");
 const report = document.querySelector(".report");
+const reportOperationList = document.querySelector(".report__operation-list");
+const reportDates = document.querySelector(".report__dates");
 
 let amount = 0;
 financeAmount.textContent = amount;
@@ -25,6 +34,19 @@ financeForm.addEventListener("submit", (e) => {
 
 OverlayScrollbars(report, {});
 
+const getData = async (url) => {
+  try {
+    const response = await fetch(`${API_URL}${url}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Ошибка при получении данных:", error);
+    throw error;
+  }
+};
+
 const closeReport = ({ target }) => {
   if (
     target.closest(".report__close") ||
@@ -40,4 +62,58 @@ const openReport = () => {
   document.addEventListener("click", closeReport);
 };
 
-financeReport.addEventListener("click", openReport);
+const reformatDate = (dateStr) => {
+  const [year, month, day] = dateStr.split("-");
+  return `${day.padStart(2, "0")}.${month.padStart(2, "0")}.${year}`;
+};
+
+const renderReport = (data) => {
+  reportOperationList.textContent = "";
+
+  const reportRows = data.map(
+    ({ category, amount, description, date, type }) => {
+      const reportRow = document.createElement("tr");
+      reportRow.classList.add("report__row");
+
+      reportRow.innerHTML = `
+                <td class="report__cell">${category}</td>
+                <td class="report__cell" style="text-align: right">${amount.toLocaleString()}&nbsp;₽</td>
+                <td class="report__cell">${description}</td>
+                <td class="report__cell">${reformatDate(date)}</td>
+                <td class="report__cell">${typeOperation[type]}</td>
+                <td class="report__action-cell">
+                  <button
+                    class="report__button report__button_table">&#10006;</button>
+                </td>
+    `;
+      return reportRow;
+    },
+  );
+
+  reportOperationList.append(...reportRows);
+};
+
+financeReport.addEventListener("click", async () => {
+  openReport();
+  const data = await getData("/test");
+  renderReport(data);
+});
+
+reportDates.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = Object.fromEntries(new FormData(reportDates));
+
+  const searchParams = new URLSearchParams();
+  if (formData.startDate) {
+    searchParams.append("startDate", formData.startDate);
+  }
+  if (formData.endDate) {
+    searchParams.append("endDate", formData.endDate);
+  }
+
+  const queryString = searchParams.toString();
+  const url = queryString ? `/test?${queryString}` : "/test";
+  const data = await getData(url);
+  console.log("data:", data);
+  renderReport(data);
+});
